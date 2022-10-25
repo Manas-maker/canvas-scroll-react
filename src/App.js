d54de04d1f4b0147bf9135f356658bdccfc3e7b4
Manas Pradhan <manasmpradhan5@gmail.com>
2022-10-26 05:20:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import CanvasScroll from './components/canvas-scroll-clip'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CanvasScroll
+        firstFrame="https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/spatial-audio/large/0001.jpg"
+        frameCount="201"
+        frameScrollArea="2000"
+      />
     </div>
   );
 }
